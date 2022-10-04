cb592b01ea0f86ebf94ff29371ce4c29f99ab82b
Justin Mei <imjustinmei@gmail.com>
2022-10-03 23:31:39 -0400

    add password functionality

--- content.js
+++ content.js
@@ -1,7 +1,6 @@
-var { period, index, focus } = JSON.parse(document.currentScript.dataset.data);
+var { period, index, password } = JSON.parse(document.currentScript.dataset.data);
 ShowClass('p' + period);
 document.getElementsByName('classes')[0].value = 'p' + String(period);
 let props = students[index].split('|');
 document.getElementsByName('students')[0].value = props[1] + ';' + props[2];
-
-if (focus) document.getElementsByTagName('input')[0].focus();
+document.getElementsByName('password')[0].value = password;
--- start.js
+++ start.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', () => {
-  chrome.storage.sync.get(['period', 'index', 'active', 'focus'], (values) => {
+  chrome.storage.sync.get(['period', 'index', 'active', 'password'], (values) => {
     if (!values['active']) return;
 
     var s = document.createElement('script');
-    s.dataset.data = JSON.stringify({ period: values['period'], index: values['index'], focus: values['focus'] });
+    s.dataset.data = JSON.stringify({ period: values['period'], index: values['index'], focus: values['focus'], password: values['password'] });
     s.src = chrome.runtime.getURL('content.js');
     s.onload = function () {
       this.remove();
