34f69ecf9b2a640a7a74be3900373daba5d6b215
Justin Mei <imjustinmei@gmail.com>
2022-10-03 23:31:07 -0400

    add password field

--- popup/popup.js
+++ popup/popup.js
@@ -1,58 +1,46 @@
-let periodSaved = (indexSaved = false);
+let saved = false;
 
-const handlePeriodSubmit = () => {
+const handleSave = () => {
   let period = document.getElementById('periodInput').value;
-  if (period == '' || period.toUpperCase() != period.toLowerCase()) return;
+  let index = document.getElementById('indexInput').value;
+  let password = document.getElementById('passwordInput').value;
+  if (!period || period.toUpperCase() != period.toLowerCase() || !index || index.toUpperCase() != index.toLowerCase() || password == 'password') return;
 
   period = Math.min(period, 10);
-  updateValue('period', period);
-};
-
-const handleIndexSubmit = () => {
-  let index = document.getElementById('indexInput').value;
-  if (index == '' || index.toUpperCase() != index.toLowerCase()) return;
-
   index = Math.min(index, 170);
+  updateValue('period', period);
   updateValue('index', index);
+  updateValue('password', password);
 };
 
 const updateValue = (name, value) => {
-  value = Math.max(0, value);
+  if (name != 'password') value = Math.max(0, value);
   chrome.storage.sync.set({ [name]: value }, function () {
-    if (window[name + 'Saved']) return;
-    window[name + 'Saved'] = true;
+    if (window['saved']) return;
+    window['saved'] = true;
 
     document.getElementById(name + 'Input').value = value;
 
-    let button = document.getElementById(name);
+    let button = document.getElementById('save');
     button.innerHTML = 'Saved!';
     button.classList.add('saved');
 
     setTimeout(() => {
       button.innerHTML = 'Save';
       button.classList.remove('saved');
-      window[name + 'Saved'] = false;
+      window['saved'] = false;
     }, 1000);
   });
 };
 
-const toggleCheckbox = (name) => {
-  chrome.storage.sync.set({ [name]: document.getElementById(name).checked });
-};
-
-document.getElementById('period').addEventListener('click', handlePeriodSubmit);
-document.getElementById('index').addEventListener('click', handleIndexSubmit);
-
+document.getElementById('save').addEventListener('click', handleSave);
 document.getElementById('active').addEventListener('click', () => {
-  toggleCheckbox('active');
-});
-document.getElementById('focus').addEventListener('click', () => {
-  toggleCheckbox('focus');
+  chrome.storage.sync.set({ active: document.getElementById('active').checked });
 });
 
-chrome.storage.sync.get(['period', 'index', 'active', 'focus'], (values) => {
+chrome.storage.sync.get(['period', 'index', 'active', 'password'], (values) => {
   document.getElementById('periodInput').value = !values['period'] ? '' : values['period'];
   document.getElementById('indexInput').value = !values['index'] ? '' : values['index'];
+  document.getElementById('passwordInput').value = !values['password'] ? '' : 'password';
   document.getElementById('active').checked = values['active'];
-  document.getElementById('focus').checked = values['focus'];
 });
